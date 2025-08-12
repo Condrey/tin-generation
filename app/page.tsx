47a7f13201c7c233fda4adff3858266bc630d9cd
Condrey <wangzdrey@gmail.com>
2025-08-12 23:08:38 +0300

    stylish

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,23 @@ import { webDescription } from "@/lib/utils";
 import { Suspense } from "react";
 import { getAllStaffs } from "./action";
 import ListOfStaffs from "./list-of-staffs";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="min-h-dvh w-full flex flex-col">
-      <main className="h-full space-y-12 flex-1 w-full py-4 max-w-5xl mx-auto ">
-        <section>
-          <h1 className='text-xl md:text-2xl font-bold tracking-tighter text-center before:content-["TIN"] md:before:content-["TaxPayer_Identification_Number"] before:mr-2 '>
+      <main className="h-full space-y-12 flex-1 w-full py-4 max-w-5xl mx-auto px-3 ">
+        <section className="flex flex-row items-center justify-evenly border-b-2 pb-2 ">
+          <Image src={'/logo.png'} alt="logo" width={85} height={85}/>
+         <div> <h1 className='text-xl md:text-2xl font-bold tracking-tighter uppercase md:capitalize text-center before:content-["TIN"] sm:before:content-["TaxPayer_Identification_Number"] before:mr-2 '>
             Collection
           </h1>
-          <h6 className="text-xs text-muted-foreground text-center max-w-prose w-full mx-auto">
+          <h6 className="text-sm text-foreground text-center max-w-prose w-full mx-auto">
             Please search if your name is available in this list and include
-            your TIN
-          </h6>
+            your TIN (TaxPayer Identification Number)
+          </h6></div>
+                    <Image src={'/coat-of-arms.png'} alt="logo" width={85} height={85}/>
+
         </section>
         <section>
           <Suspense fallback={<DataTableLoadingSkeleton />}>
@@ -23,7 +27,7 @@ export default function Home() {
           </Suspense>
         </section>
       </main>
-      <footer className="bg-gradient-to-l space-y-2 from-secondary to-muted text-secondary-foreground px-4 w-full py-2">
+      <footer className="bg-gradient-to-l space-y-2 text-slate-200 from-slate-800 via-slate-500 to-slate-800  px-4 w-full py-2">
         <p className="text-center max-w-xl mx-auto w-full text-xs">
           {webDescription}
         </p>
